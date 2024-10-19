d2e6aa799eec4ccbe42e39c0048dc549731e2dfc
georges-kayembe_wcar <georges.kayembe@wirelesscar.com>
2024-10-20 00:48:30 +0200

    minor changes in the footbar

--- client/src/view/components/footer.js
+++ client/src/view/components/footer.js
@@ -8,10 +8,10 @@ function Footer() {
         <footer>
             <div className='footer'>
                 <p>Follow us on</p>
-                <a href='https://github.com/gmiak'><i className="pi pi-facebook"></i></a>
-                <a href='https://github.com/gmiak'><i className="pi pi-instagram"></i></a>
-                <a href='https://github.com/gmiak'><i className="pi pi-twitter"></i></a>
-                <a href='https://github.com/gmiak'><i className="pi pi-github"></i></a>
+                <a href='https://facebook.com/georgeskayembemiaka'><i className="pi pi-facebook"></i></a>
+                <a href='https://instagram.com/georgesmiaka2'><i className="pi pi-instagram"></i></a>
+                <a href='https://twitter.com/gmiak_dv'><i className="pi pi-twitter"></i></a>
+                <a href='https://github.com/georgesmiaka'><i className="pi pi-github"></i></a>
                 <p className='copyright'>2022 &copy; Group46, All rights reserved.</p>
             </div>
         </footer></>
